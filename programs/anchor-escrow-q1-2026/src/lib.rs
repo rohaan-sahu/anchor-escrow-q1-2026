68dd9bddefc06e2486313d2a760ff881283f8d03
rohaan-sahu <rbbsr8292@gmail.com>
2026-02-06 20:20:27 +0000

    Initial commit
    Tests not yet passing

---
+++ programs/anchor-escrow-q1-2026/src/lib.rs
@@ -0,0 +1,42 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+
+
+declare_id!("EgDVo2wVRW1LGSbBJBUtCEvswBnU3v6inYJcNwE34B8x");
+
+#[program]
+pub mod anchor_escrow_q1_2026 {
+    use super::*;
+
+    pub fn make(ctx: Context<Make>,seed: u64, deposit: u64, receive: u64) -> Result<()> {
+        
+        ctx.accounts.init_escrow(seed, receive, &ctx.bumps)?;
+
+        ctx.accounts.deposit(deposit)?;
+
+        Ok(())
+    }
+
+    pub fn refund(ctx: Context<Refund>,seed: u64) -> Result<()> {
+
+        ctx.accounts.refund(seed)?;
+
+        Ok(())
+    }
+
+    pub fn take(ctx: Context<Take>) -> Result<()>{
+
+        ctx.accounts.take()?;
+
+        Ok(())
+    }
+}
